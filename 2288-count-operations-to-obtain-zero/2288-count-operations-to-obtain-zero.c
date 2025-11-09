b8d64f6651cc2353f521eb3818f0cced626bd01f
Vanthiyath thevan A <131964028+vanthiyan7@users.noreply.github.com>
2025-11-09 11:03:20 +0530

    Time: 3 ms (8.51%), Space: 7.8 MB (89.36%) - LeetHub

---
+++ 2288-count-operations-to-obtain-zero/2288-count-operations-to-obtain-zero.c
@@ -0,0 +1,21 @@
+int countOperations(int num1, int num2) {
+    int c=0;
+    while(num1 && num2)
+    {
+        if(num1<num2)
+        {
+            num2 = num2 - num1;
+            num1 = num1;
+            c++;
+        }
+        else if(num1>num2)
+        {
+            num1 = num1 - num2;
+            num2 = num2;
+            c++;
+        }
+        else
+        return c+1;
+    }
+    return c;
+}
