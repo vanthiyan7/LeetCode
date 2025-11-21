47d8f4ba0567b05d7573bd668769eaad043d1161
Vanthiyath thevan A <131964028+vanthiyan7@users.noreply.github.com>
2025-11-21 12:16:14 +0530

    Time: 11 ms (92.77%), Space: 47.2 MB (85.83%) - LeetHub

---
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        if(nums.length == k)
+        return nums;
+        Queue<int[]> minheap=new PriorityQueue<>((a,b)->{return a[0]-b[0];});
+        Map<Integer,Integer> map = new HashMap<>();
+        for(int n:nums)
+        map.merge(n,1,Integer::sum);
+        for(Map.Entry<Integer,Integer> m:map.entrySet())
+        {
+            minheap.add(new int[] {m.getValue(),m.getKey()});
+            if(minheap.size()>k)
+                minheap.remove();
+        }
+        int[] ans=new int[k];
+        int j=0;
+        while(!minheap.isEmpty())
+        {
+            ans[j++]=minheap.remove()[1];
+        }
+        return ans;
+
+    }
+}
