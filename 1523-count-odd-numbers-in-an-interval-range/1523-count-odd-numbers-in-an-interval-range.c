87d92dd7f444851e872a7ecde71bc98a0cd88e6f
Vanthiyath thevan A <131964028+vanthiyan7@users.noreply.github.com>
2025-12-07 17:19:58 +0530

    Time: 1738 ms (24.09%), Space: 7.8 MB (19.94%) - LeetHub

---
+++ 1523-count-odd-numbers-in-an-interval-range/1523-count-odd-numbers-in-an-interval-range.c
@@ -0,0 +1,15 @@
+
+
+int countOdds(int low, int high){
+    int c=0;
+    while (low <= high)
+    {
+        if(low % 2 != 0)
+        {
+            c++; 
+        }  
+        low++; 
+    }
+    return c;
+
+}
